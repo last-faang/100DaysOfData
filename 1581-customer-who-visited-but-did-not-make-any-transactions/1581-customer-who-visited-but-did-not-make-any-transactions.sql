4eb940815b3908a585a578626d97db646d414f43
last-faang <dmhuh.dev@gmail.com>
2024-06-30 00:05:43 +0900

    Time: 1054 ms (30.31%), Space: 0B (100%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,8 @@
+SELECT
+    Visits.customer_id,
+    COUNT(*) AS count_no_trans
+FROM Visits
+LEFT JOIN Transactions
+    ON Visits.visit_id = Transactions.visit_id
+WHERE Transactions.transaction_id is NULL
+GROUP BY Visits.customer_id;
