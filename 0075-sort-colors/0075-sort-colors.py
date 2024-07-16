f6248a0cbdbe69a20ca1748ed5601b0add59e807
last-faang <dmhuh.dev@gmail.com>
2024-07-16 23:03:14 +0900

    Time: 46 ms (6.26%), Space: 16.6 MB (16.93%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -5,18 +5,13 @@ class Solution:
         """
         left, current, right = 0, 0, len(nums) - 1
 
-        while current < right:
+        while current <= right:
             if nums[current] == 0:
                 nums[left], nums[current] = nums[current], nums[left]
                 left += 1
-            if nums[current] == 2:
+                current += 1
+            elif nums[current] == 2:
                 nums[right], nums[current] = nums[current], nums[right]
                 right -= 1
-            if nums[right] == 0:
-                nums[right], nums[left] = nums[left], nums[right]
-                left += 1
-            if nums[left] == 2:
-                nums[right], nums[left] = nums[left], nums[right]
-                right -= 1
-
-            current += 1
+            else:
+                current += 1
