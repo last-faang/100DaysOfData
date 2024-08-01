a63d36a3d6f92d5619a17e352593d659905ab62a
last-faang <dmhuh.dev@gmail.com>
2024-08-01 23:47:42 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1412-find-the-quiet-students-in-all-exams/1412-find-the-quiet-students-in-all-exams.sql
@@ -0,0 +1,22 @@
+WITH min_max_list AS (
+    SELECT 
+    exam_id,
+    MIN(score) AS min_score,
+    MAX(score) AS max_score
+    FROM Exam
+    GROUP BY exam_id
+)
+
+SELECT 
+    Student.student_id,
+    Student.student_name
+FROM exam
+LEFT JOIN Student
+    USING (student_id)
+LEFT JOIN min_max_list
+    ON Exam.exam_id = min_max_list.exam_id
+    AND (Exam.score = min_max_list.min_score OR Exam.score = min_max_list.max_score)
+GROUP BY
+    Student.student_id,
+    Student.student_name  
+HAVING COUNT(min_max_list.exam_id) = 0;
